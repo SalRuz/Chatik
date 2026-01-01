2f29ebbe890d4f5c0ce469c90194c0fa9b4a669d
SalRuz <ruzelsalmanov1@gmail.com>
2026-01-02 00:43:40 +0500

    Delete get_messages.php

--- get_messages.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: text/html; charset=utf-8');
-
-if (file_exists('messages.txt')) {
-    echo file_get_contents('messages.txt');
-} else {
-    echo "<em>Сообщений пока нет.</em>";
-}
-?>
